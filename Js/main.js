a5ebefefb5ad6e5c62183d4a71dc49e79e1ec64b
n1nurrr <119728897+n1nurrr@users.noreply.github.com>
2022-12-08 23:49:41 +0600

    Add files via upload
    завершние работы над скриптом с этажами

--- Js/main.js
+++ Js/main.js
@@ -1,13 +1,35 @@
 $(document).ready(function () {
     var currentFloor = 2;
+    var floorPath = $(".home-image path"); //выделенные этажи svg
     var counterUp= $(".counter-up");
-    $(".home-image path").on('mouseover', function(){
-        currentFloor = $(this).attr('data-floor');
-        $(".counter").text(currentFloor);
+    var counterDown= $(".counter-down");
+
+    //Наведение мышкой на этаж
+    floorPath.on('mouseover', function(){
+        floorPath.removeClass("current-floor") // remove current floor class
+        currentFloor = $(this).attr('data-floor'); // значение текущего этажа
+        $(".counter").text(currentFloor); //запись активного значения этажа в счетчик справа
     });
+
+    //нажатие кнопки "Вверх"
     counterUp.on('click', function(){
-        currentFloor++;
-        usCurrentFloor = currentFloor.toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false});
-        $(".counter").text(usCurrentFloor);
+        if(currentFloor <18){
+            currentFloor++;
+            usCurrentFloor = currentFloor.toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false});
+            $(".counter").text(usCurrentFloor);
+            floorPath.removeClass("current-floor") // remove current floor class
+            $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor");
+        }
+    })
+
+    //нажатие кнопки "Вниз"    
+    counterDown.on('click', function(){
+        if(currentFloor >2){
+            currentFloor--;
+            usCurrentFloor = currentFloor.toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false});
+            $(".counter").text(usCurrentFloor);
+            floorPath.removeClass("current-floor") // remove current floor class
+            $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor");
+        }
     })
 });
